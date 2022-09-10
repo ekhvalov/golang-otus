cd1a2797e35d3a85f27811896b2ab614015f36a4
user <ekhvalov@gmail.com>
2022-09-10 14:22:51 +0900

    main implemented.

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	args := os.Args
+	if len(args) < 3 {
+		fmt.Printf("Usage: %s <path_to_env_dir> <command>\n", args[0])
+		return
+	}
+	env, err := ReadDir(args[1])
+	if err != nil {
+		panic(err)
+	}
+	RunCmd(args[2:], env)
 }
