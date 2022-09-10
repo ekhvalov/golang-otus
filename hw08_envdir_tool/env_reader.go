563921f48f24ea334e26b9bfc8babb2815d4e6a9
user <ekhvalov@gmail.com>
2022-09-10 14:22:04 +0900

    env_reader implemented.

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"os"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -8,9 +12,84 @@ type EnvValue struct {
 	NeedRemove bool
 }
 
+const (
+	newLine  byte = '\n'
+	zeroByte byte = 0x0000
+)
+
+var rightTrimBytes = []byte{' ', '\t'}
+
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	env := Environment{}
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+		info, err := entry.Info()
+		if err != nil {
+			return nil, err
+		}
+		if info.Size() == 0 {
+			env[info.Name()] = EnvValue{NeedRemove: true}
+			continue
+		}
+		value, err := getValueFromFile(dir + "/" + info.Name())
+		if err != nil {
+			return nil, err
+		}
+		env[entry.Name()] = EnvValue{Value: value}
+	}
+	return env, nil
+}
+
+func getValueFromFile(path string) (string, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	line := getFirstLine(data)
+	return string(sanitize(line)), nil
+}
+
+func getFirstLine(data []byte) []byte {
+	for i, d := range data {
+		if d == newLine {
+			return data[:i]
+		}
+	}
+	return data
+}
+
+func sanitize(line []byte) []byte {
+	for i, b := range line {
+		if b == zeroByte {
+			line[i] = newLine
+		}
+	}
+	return trimRight(line)
+}
+
+func trimRight(line []byte) []byte {
+	for len(line) > 0 {
+		if !shouldBeTrimmed(line[len(line)-1]) {
+			return line
+		}
+		line = line[:len(line)-1]
+	}
+	return line
+}
+
+func shouldBeTrimmed(b byte) bool {
+	for _, trimByte := range rightTrimBytes {
+		if b == trimByte {
+			return true
+		}
+	}
+	return false
 }
--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,46 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	type testCase struct {
+		path        string
+		expectedEnv Environment
+	}
+	testCases := map[string]testCase{
+		"when files existed then should read variables": {
+			path: "./testdata/env",
+			expectedEnv: Environment{
+				"BAR":   {Value: "bar"},
+				"EMPTY": {Value: ""},
+				"FOO":   {Value: "   foo\nwith new line"},
+				"HELLO": {Value: "\"hello\""},
+				"UNSET": {Value: "", NeedRemove: true},
+			},
+		},
+		"when read empty dir then environment should be empty": {
+			path:        "./testdata/env-empty",
+			expectedEnv: Environment{},
+		},
+	}
+	for testName, tc := range testCases {
+		t.Run(testName, func(t *testing.T) {
+			actualEnv, err := ReadDir(tc.path)
+			require.NoError(t, err)
+			require.NotNil(t, actualEnv)
+			for key, value := range tc.expectedEnv {
+				actualValue, ok := actualEnv[key]
+				require.Truef(t, ok, "no value for key: %s", key)
+				require.Equalf(t, value.Value, actualValue.Value, "wrong value for key: %s", key)
+			}
+			for key := range actualEnv {
+				_, ok := tc.expectedEnv[key]
+				require.Truef(t, ok, "key %s is present but should not")
+			}
+		})
+	}
 }
