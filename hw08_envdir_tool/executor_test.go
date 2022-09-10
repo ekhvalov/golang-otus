db720d57c2c97b1e3e7c418ff95f60221ebbdf81
user <ekhvalov@gmail.com>
2022-09-10 14:22:44 +0900

    executor implemented.

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,39 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	type testCase struct {
+		cmd      []string
+		env      Environment
+		exitCode int
+	}
+
+	testCases := map[string]testCase{
+		"direct exit code": {
+			cmd:      []string{"sh", "-c", "exit 10"},
+			env:      Environment{},
+			exitCode: 10,
+		},
+		"exit code from environment variable": {
+			cmd:      []string{"sh", "-c", "exit $EXIT_CODE"},
+			env:      Environment{"EXIT_CODE": EnvValue{Value: "101"}},
+			exitCode: 101,
+		},
+		"plain exit": {
+			cmd:      []string{"sh"},
+			env:      Environment{},
+			exitCode: 0,
+		},
+	}
+	for testName, tc := range testCases {
+		t.Run(testName, func(t *testing.T) {
+			actualExitCode := RunCmd(tc.cmd, tc.env)
+			require.Equal(t, tc.exitCode, actualExitCode)
+		})
+	}
 }
