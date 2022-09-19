e1619512cc7fc850375579a1f00fb27d05c96041
user <ekhvalov@gmail.com>
2022-09-19 12:06:48 +0900

    Complex logic simplified.

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -23,13 +23,16 @@ func Run(tasks []Task, workersLimit, errorsLimit int) error {
 	wg := sync.WaitGroup{}
 	tasksCh := make(chan Task)
 	errorsCh := make(chan error, workersLimit)
+	doneCh := make(chan struct{})
 	for i := 0; i < workersLimit; i++ {
 		wg.Add(1)
 		go handleTasks(tasksCh, errorsCh, &wg)
 	}
-	result := produce(tasks, tasksCh, errorsCh, errorsLimit)
+	go countErrors(errorsCh, errorsLimit, doneCh)
+	result := produceTasks(doneCh, tasks, tasksCh)
 	close(tasksCh)
 	wg.Wait()
+	close(errorsCh)
 	return result
 }
 
@@ -43,28 +46,30 @@ func handleTasks(tasksCh <-chan Task, errorsCh chan<- error, wg *sync.WaitGroup)
 	wg.Done()
 }
 
-func produce(tasks []Task, tasksCh chan<- Task, errorsCh <-chan error, errorsLimit int) error {
+func countErrors(errorsCh <-chan error, errorsLimit int, doneCh chan<- struct{}) {
 	errorsCount := 0
-	for _, task := range tasks {
-		isTaskSent := false
-		for !isTaskSent {
-			if errorsLimit > 0 && errorsCount == errorsLimit {
-				return ErrErrorsLimitExceeded
-			}
-			select {
-			case <-errorsCh:
-				errorsCount++
-				continue
-			default:
-			}
-			select {
-			case <-errorsCh:
-				errorsCount++
-				continue
-			case tasksCh <- task:
-				isTaskSent = true
-			}
+	for range errorsCh {
+		errorsCount++
+		if errorsCount == errorsLimit {
+			doneCh <- struct{}{}
+			// don't break to drain remain errors
 		}
+	}
+}
+
+func produceTasks(doneCh <-chan struct{}, tasks []Task, tasksCh chan<- Task) error {
+	for _, task := range tasks {
+		select {
+		case <-doneCh:
+			return ErrErrorsLimitExceeded
+		default:
+		}
+		select {
+		case tasksCh <- task:
+		case <-doneCh:
+			return ErrErrorsLimitExceeded
+		}
+
 	}
 	return nil
 }
