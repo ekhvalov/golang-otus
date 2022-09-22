a2d6d9cbc922cccfae3076d14c524c17c6d11bed
user <ekhvalov@gmail.com>
2022-09-22 12:43:13 +0900

    Package name fixed.

--- hw12_13_14_15_calendar/cmd/calendar/main.go
+++ hw12_13_14_15_calendar/cmd/calendar/main.go
@@ -8,10 +8,10 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/app"
-	"github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/logger"
-	internalhttp "github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/server/http"
-	memorystorage "github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/storage/memory"
+	"github.com/ekhvalov/hw12_13_14_15_calendar/internal/app"
+	"github.com/ekhvalov/hw12_13_14_15_calendar/internal/logger"
+	internalhttp "github.com/ekhvalov/hw12_13_14_15_calendar/internal/server/http"
+	memorystorage "github.com/ekhvalov/hw12_13_14_15_calendar/internal/storage/memory"
 )
 
 var configFile string
