59c80f019463079b0f7e93f75df54aaefb97b1c3
user <ekhvalov@gmail.com>
2022-09-30 12:14:08 +0900

    Gomock used for delete event command.

--- hw12_13_14_15_calendar/internal/app/event/command/delete_event.go
+++ hw12_13_14_15_calendar/internal/app/event/command/delete_event.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/ekhvalov/hw12_13_14_15_calendar/internal/domain/event"
 )
@@ -16,6 +17,13 @@ type DeleteEventRequestHandler interface {
 
 type deleteEventRequestHandler struct {
 	repository event.Repository
+}
+
+func NewDeleteEventRequestHandler(repository event.Repository) (DeleteEventRequestHandler, error) {
+	if repository == nil {
+		return nil, fmt.Errorf("provided repository is nil")
+	}
+	return deleteEventRequestHandler{repository: repository}, nil
 }
 
 func (h deleteEventRequestHandler) Handle(ctx context.Context, request DeleteEventRequest) error {
--- hw12_13_14_15_calendar/internal/app/event/command/delete_event_test.go
+++ hw12_13_14_15_calendar/internal/app/event/command/delete_event_test.go
@@ -1,52 +1,69 @@
-package command
+package command_test
 
 import (
 	"context"
+	"errors"
 	"testing"
 
+	"github.com/ekhvalov/hw12_13_14_15_calendar/internal/app/event/command"
 	"github.com/ekhvalov/hw12_13_14_15_calendar/internal/domain/event"
+	"github.com/ekhvalov/hw12_13_14_15_calendar/internal/domain/event/mock"
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 )
 
-func Test_deleteEventRequestHandler_Handle(t *testing.T) {
-	type fields struct {
-		repository event.Repository
+func Test_deleteEventRequestHandler_Handle_Error(t *testing.T) {
+	getMockRepository := func(controller *gomock.Controller) event.Repository {
+		return mock.NewMockRepository(controller)
 	}
+	eventID := "100500"
+	errDelete := errors.New("delete event error")
 	tests := map[string]struct {
-		fields      fields
-		request     DeleteEventRequest
-		wantErr     bool
-		wantEventID string
+		request       command.DeleteEventRequest
+		getRepository func(controller *gomock.Controller) event.Repository
 	}{
-		"should return error when event ID is empty": {
-			request: DeleteEventRequest{ID: ""},
-			wantErr: true,
+		"event ID is empty": {
+			request:       command.DeleteEventRequest{ID: ""},
+			getRepository: getMockRepository,
 		},
 		"should return error when event repository returned error": {
-			request: DeleteEventRequest{ID: "100500"},
-			fields:  fields{repository: event.BrokenRepository{}},
-			wantErr: true,
-		},
-		"should return no error when everything is correct": {
-			request:     DeleteEventRequest{ID: "100500"},
-			fields:      fields{repository: &event.PlainRepository{}},
-			wantErr:     false,
-			wantEventID: "100500",
+			request: command.DeleteEventRequest{ID: eventID},
+			getRepository: func(controller *gomock.Controller) event.Repository {
+				r := mock.NewMockRepository(controller)
+				r.EXPECT().
+					Delete(context.Background(), eventID).
+					Return(errDelete)
+				return r
+			},
 		},
 	}
+
 	for testName, tt := range tests {
 		t.Run(testName, func(t *testing.T) {
-			d := deleteEventRequestHandler{
-				repository: tt.fields.repository,
-			}
-			err := d.Handle(context.Background(), tt.request)
-			if tt.wantErr {
-				require.Error(t, err)
-				return
-			}
+			controller := gomock.NewController(t)
+			defer controller.Finish()
+
+			h, err := command.NewDeleteEventRequestHandler(tt.getRepository(controller))
+
 			require.NoError(t, err)
-			r := tt.fields.repository.(*event.PlainRepository)
-			require.Equal(t, tt.wantEventID, r.EventID)
+			err = h.Handle(context.Background(), tt.request)
+			require.Error(t, err)
 		})
 	}
+}
+
+func Test_deleteEventRequestHandler_Handle(t *testing.T) {
+	eventID := "100500"
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+	repository := mock.NewMockRepository(controller)
+	repository.EXPECT().
+		Delete(context.Background(), eventID).
+		Return(nil)
+
+	h, err := command.NewDeleteEventRequestHandler(repository)
+
+	require.NoError(t, err)
+	err = h.Handle(context.Background(), command.DeleteEventRequest{ID: eventID})
+	require.NoError(t, err)
 }
